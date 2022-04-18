1abc8db6e7a0459de43e8e9cb161d8cd970a9b32
taron0503 <taron0503@gmail.com>
2022-04-18 14:31:05 -0400

    Initial commit

---
+++ app/src/main/java/com/example/alarmapp/bindingAdapters/EditTextBindingAdapter.java
@@ -0,0 +1,16 @@
+package com.example.alarmapp.bindingAdapters;
+
+import android.net.Uri;
+import android.widget.TextView;
+
+import androidx.databinding.BindingAdapter;
+
+public class EditTextBindingAdapter {
+    @BindingAdapter({"app:soundTitle"})
+    public static void soundTitle(TextView textView, String soundPath) {
+        Uri soundUri = Uri.parse(soundPath);
+        String title = soundUri.getQueryParameter("title");
+        textView.setText(title != null?title:"Default Sound");
+
+    }
+}
